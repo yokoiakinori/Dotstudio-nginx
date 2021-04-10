fb769b504c729faae2e2f6d370fb1c6b65d91a53
yokoiakinori <yokoiakinori@icloud.com>
2021-04-10 11:14:35 +0900

    リクエストファクトリ修正

--- server/laravel/database/factories/MaterialRequestFactory.php
+++ server/laravel/database/factories/MaterialRequestFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(MaterialRequest::class, function (Faker $faker) {
-    $title = $faker->array();
-    $contents = $faker->sentence();
+    $title = $faker->array;
+    $contents = $faker->sentence;
     return [
         'title' => $title,
         'contents' => $contents,
